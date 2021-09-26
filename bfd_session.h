ab133d2c4d6d950a3e5a10a428a902eb423c1fbd
Beniamin Sandu <beniaminsandu@gmail.com>
2021-09-26 19:09:48 +0200

    bfd_session.h: create initial header
    Signed-off-by: Beniamin Sandu <beniaminsandu@gmail.com>

---
+++ bfd_session.h
@@ -0,0 +1,10 @@
+#ifndef BFD_SESSION_H_
+#define BFD_SESSION_H_
+
+#define BFD_CTRL_PORT   3784
+
+/* Any chance we get 16384 active BFD sessions? :) */
+#define BFD_SRC_PORT_MIN    49152
+#define BFD_SRC_PORT_MAX    65535
+
+#endif //BFD_SESSION_H_
