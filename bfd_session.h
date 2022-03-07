de9d291e2f773d488ef01b5841dc096230d7af1b
Beniamin Sandu <beniaminsandu@gmail.com>
2022-03-07 21:15:19 +0200

    add include guards to all headers and license info to all source files
    Signed-off-by: Beniamin Sandu <beniaminsandu@gmail.com>

--- bfd_session.h
+++ bfd_session.h
@@ -1,5 +1,31 @@
-#ifndef BFD_SESSION_H_
-#define BFD_SESSION_H_
+/*
+ * Copyright (C) 2022 Beniamin Sandu <beniaminsandu@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef BFD_SESSION_H
+#define BFD_SESSION_H
 
 #include <stdbool.h>
 #include <arpa/inet.h>
@@ -94,4 +120,4 @@ struct bfd_session {
 bfd_session_id bfd_session_start(struct bfd_session_params *params);
 void bfd_session_stop(bfd_session_id session_id);
 
-#endif //BFD_SESSION_H_
+#endif //BFD_SESSION_H
