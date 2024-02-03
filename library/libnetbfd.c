0992112f8d439b101f80c5486a9c1bf84da100e7
Beniamin Sandu <beniaminsandu@gmail.com>
2024-02-03 14:52:23 +0000

    library: add current diagnostic code in session stats info
    Signed-off-by: Beniamin Sandu <beniaminsandu@gmail.com>

--- library/libnetbfd.c
+++ library/libnetbfd.c
@@ -244,6 +244,7 @@ void bfd_session_print_stats(bfd_session_id session_id)
     printf("%-25s %d\n", "Detection Multiplier:", session->session_params->detect_mult);
     printf("%-25s 0x%x\n", "My discriminator:", session->current_session->local_discr);
     printf("%-25s %s\n", "Current state:", bfd_state2string(session->current_session->local_state));
+    printf("%-25s %s\n", "Current diag:", bfd_diag2string(session->current_session->local_diag));
     printf("%-25s %d\n", "Operational TX:", session->current_session->op_tx);
     printf("%-25s %lu\n", "Detection time:", session->current_session->detection_time);
 #ifdef DEBUG_ENABLE
@@ -305,6 +306,7 @@ void bfd_session_print_stats_log(bfd_session_id session_id)
     fprintf(file, "%-25s %d\n", "Detection Multiplier:", session->session_params->detect_mult);
     fprintf(file, "%-25s 0x%x\n", "My discriminator:", session->current_session->local_discr);
     fprintf(file, "%-25s %s\n", "Current state:", bfd_state2string(session->current_session->local_state));
+    fprintf(file, "%-25s %s\n", "Current diag:", bfd_diag2string(session->current_session->local_diag));
     fprintf(file, "%-25s %d\n", "Operational TX:", session->current_session->op_tx);
     fprintf(file, "%-25s %lu\n", "Detection time:", session->current_session->detection_time);
     fprintf(file, "---------------------------------------------\n");
